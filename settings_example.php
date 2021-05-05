6bd3abd3a66450c2e2de3762a0d5eeb56c040d93
SapphireCascade <codeychase64@gmail.com>
2021-05-05 01:50:09 +0100

    added explanations to settings file

--- settings_example.php
+++ settings_example.php
@@ -1,4 +1,5 @@
 <?php
+	// rename this file to settings.php
 	function setup(){
 		settings("MAIN_SITE_URL","");
 		settings("OPEN_URLS",array(""));
@@ -8,16 +9,16 @@
 		settings("ADDITIONAL_PASSWORD_KEY","");//makes passwords more secure, don't delete environmental variable otherwise everyone will have to reset their password
 		settings("IS_SELF_KEY", "");//create a cookie called is_self and store a random string, create an environment variable with the same string and pass this environment variable through this function
 		settings("CACHELESS_URL", "");// can be as a string or an array of strings. Javascript and CSS won't cache on these sites (development purposes only)
-		settings("DB_SERVER_NAME", "");
-		settings("DB_USERNAME", "");
-		settings("DB_PASSWORD", "");
-		settings("DB_PORT",0);
-		settings("DB_DBNAME","");
-		settings("EMAIL_HOST", "");
-		settings("EMAIL_USERNAME", "");
-		settings("EMAIL_PASSWORD", "");
-		settings("EMAIL_ADDRESS_FROM", "");
-		settings("EMAIL_NAME_FROM", "");
+		settings("DB_SERVER_NAME", "");// ip address of the database
+		settings("DB_USERNAME", "");// username to access the database
+		settings("DB_PASSWORD", "");// password to access the database
+		settings("DB_PORT",0);// port of the database (standard is 3306)
+		settings("DB_DBNAME","");// name of the database
+		settings("EMAIL_HOST", "");// url to send emails through e.g. smtp.eu.mailgun.org
+		settings("EMAIL_USERNAME", "");// email address username e.g. postmaster@mail.site.com
+		settings("EMAIL_PASSWORD", "");// email address password
+		settings("EMAIL_ADDRESS_FROM", "");// what gets displayed in the from on recievers email clients
+		settings("EMAIL_NAME_FROM", "");// what gets displayed in the from on recievers email clients
 		settings("ERROR_NAME", "");// Who to contact when the user sees an error
 	}
 ?>
